b5838d7edd0edde1ca4e7e485e84129cc2f43ccc
WINDOWSTEN <WINDOWSTEN@DESKTOP-6D9830K>
2019-10-16 16:24:50 +0530

    momiz44

---
+++ Spring2/src/com/Test2.java
@@ -0,0 +1,17 @@
+package com;  
+  
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test2 {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("applicationContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("e");  
+        s.show();  
+          
+    }  
+}  
